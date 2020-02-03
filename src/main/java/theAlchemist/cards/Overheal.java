194994c12df923a3f5e13b2f6fefaa245b9d7afc
Jolkert <poptartking86@gmail.com>
2020-02-02 21:21:10 -0500

    Implemented Overheal with placeholder art. The implementation could probably be improved, but it works so it's fine.

--- src/main/java/theAlchemist/cards/Overheal.java
+++ src/main/java/theAlchemist/cards/Overheal.java
@@ -3,6 +3,7 @@ package theAlchemist.cards;
 import basemod.abstracts.CustomCard;
 import basemod.helpers.BaseModCardTags;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.actions.common.DamageAction;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
@@ -12,40 +13,36 @@ import com.megacrit.cardcrawl.localization.CardStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import theAlchemist.AlchemistMod;
 import theAlchemist.characters.TheAlchemist;
+import theAlchemist.powers.OverhealPower;
 
 import static theAlchemist.AlchemistMod.makeCardPath;
 
 public class Overheal extends CustomCard
 {// Template for a basic strike card
-	public static final String ID = AlchemistMod.makeID(AttackTemplate.class.getSimpleName());
-	public static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
+	public static final String ID = AlchemistMod.makeID(Overheal.class.getSimpleName());
+	public static final CardStrings CARD_STRINGS = CardCrawlGame.languagePack.getCardStrings(ID);
 
 	public static final String IMG = makeCardPath("Overheal.png");
-	public static final String NAME = cardStrings.NAME;
-	public static final String DESCRIPTION = cardStrings.DESCRIPTION;
+	public static final String NAME = CARD_STRINGS.NAME;
+	public static final String DESCRIPTION = CARD_STRINGS.DESCRIPTION;
+	public static final String UPGRADE_DESCRIPTION = CARD_STRINGS.DESCRIPTION;
 
 	private static final CardRarity RARITY = CardRarity.UNCOMMON;
-	private static final CardTarget TARGET = CardTarget.ENEMY;
-	private static final CardType TYPE = CardType.ATTACK;
+	private static final CardTarget TARGET = CardTarget.SELF;
+	private static final CardType TYPE = CardType.POWER;
 	private static final CardColor COLOR = TheAlchemist.Enums.COLOR_PLATINUM;
 
 	private static final int COST = 2;
-	private static final int DAMAGE = 6;
-	private static final int UPGRADE_PLUS_DMG = 3; //upgrade
 
 	public Overheal()
 	{
 		super(ID, NAME, IMG, COST, DESCRIPTION, TYPE, COLOR, RARITY, TARGET);
-		
-		baseDamage = DAMAGE;
-		this.tags.add(BaseModCardTags.BASIC_STRIKE);
-		this.tags.add(CardTags.STARTER_STRIKE);
 	}
 	
 	@Override
 	public void use(AbstractPlayer player, AbstractMonster monster)
 	{
-		AbstractDungeon.actionManager.addToBottom(new DamageAction(monster, new DamageInfo(player, damage, damageTypeForTurn), AbstractGameAction.AttackEffect.SLASH_HORIZONTAL));
+		AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(player, player, new OverhealPower(player)));
 	}
 	
 	@Override
@@ -54,7 +51,8 @@ public class Overheal extends CustomCard
 		if(!upgraded)
 		{
 			upgradeName();
-			upgradeDamage(UPGRADE_PLUS_DMG);
+			this.isInnate = true;
+			this.rawDescription = UPGRADE_DESCRIPTION + DESCRIPTION;
 			initializeDescription();
 		}
 	}
---
+++ src/main/java/theAlchemist/powers/OverhealPower.java
@@ -0,0 +1,106 @@
+package theAlchemist.powers;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.PowerStrings;
+import com.megacrit.cardcrawl.powers.AbstractPower;
+import com.megacrit.cardcrawl.powers.RegenPower;
+import theAlchemist.AlchemistMod;
+import theAlchemist.util.TextureLoader;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Stack;
+
+import static theAlchemist.AlchemistMod.*;
+
+public class OverhealPower extends AbstractPower
+{
+	public AbstractCreature source;
+	
+	public static final String POWER_ID = AlchemistMod.makeID("OverhealPower");
+	private static final PowerStrings powerStrings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
+	public static final String NAME = powerStrings.NAME;
+	public static final String[] DESCRIPTIONS = powerStrings.DESCRIPTIONS;
+	
+	private Stack<AbstractCard> cardsTriggered;
+	
+	//IMAGES
+	private static final Texture TEXTURE_84 = TextureLoader.getTexture(makePowerPath("84/overheal_power.png"));
+	private static final Texture TEXTURE_32 = TextureLoader.getTexture(makePowerPath("32/overheal_power.png"));
+	
+	public OverhealPower(AbstractCreature owner)
+	{
+		this.name = NAME;
+		this.ID = POWER_ID;
+		
+		this.owner = owner;
+		
+		this.type = PowerType.BUFF;
+		this.isTurnBased = false;
+		
+		this.region128 = new TextureAtlas.AtlasRegion(TEXTURE_84, 0, 0, 84, 84);
+		this.region48 = new TextureAtlas.AtlasRegion(TEXTURE_32, 0, 0, 32, 32);
+		
+		updateDescription();
+		
+		cardsTriggered = new Stack<AbstractCard>();
+		cardsTriggered.push(getLastPlayedHealCard());
+	}
+	
+	@Override
+	public int onHeal(int amountHealed)
+	{
+		int regenToAdd = owner.currentHealth + amountHealed - owner.maxHealth;
+		
+		if(regenToAdd > 0 && lastHealWasCard())
+		{
+			flashWithoutSound();
+			AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(owner, owner, new RegenPower(owner, regenToAdd)));
+			
+			AbstractCard lastHeal = getLastPlayedHealCard();
+			if(lastHeal != null)
+				cardsTriggered.push(lastHeal);
+		}
+		
+		return amountHealed;
+		
+	}
+	
+	@Override
+	public void updateDescription()
+	{
+		this.description = DESCRIPTIONS[0];
+	}
+	
+	public AbstractCard getLastPlayedHealCard()
+	{
+		ArrayList<AbstractCard> cardsThisCombat = AbstractDungeon.actionManager.cardsPlayedThisCombat;
+		
+		AbstractCard lastPlayedHeal = null;
+		for(int i = cardsThisCombat.size() - 1; i >= 0; i--)
+			if(cardsThisCombat.get(i).hasTag(AbstractCard.CardTags.HEALING))
+			{
+				lastPlayedHeal = cardsThisCombat.get(i);
+				break;
+			}
+		
+		return lastPlayedHeal;
+	}
+	
+	public boolean lastHealWasCard()
+	{
+		AbstractCard lastPlayedHeal = getLastPlayedHealCard();
+		return cardsTriggered.peek() != lastPlayedHeal;
+	}
+	
+	public AbstractPower makeCopy()
+	{
+		return new OverhealPower(owner);
+	}
+}
